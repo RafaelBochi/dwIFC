4923edba78c895636d57f31cd620e49a210b039c
RafaelBochi <rafaelbochibarros@gmail.com>
2022-10-13 15:13:40 -0300

    Sistema backspace

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,123 +1,137 @@
-let btnNumeros = document.querySelectorAll('.numeros');
-let visor = document.getElementById('visor');
-let btnC = document.querySelector('.btn-c');
-let operador = document.querySelectorAll('.operador');
-let bntIgual = document.querySelector('#btn-igual');
-let btnPonto = document.querySelector('#btn-ponto');
-
-for (let btn of btnNumeros) {
-    btn.addEventListener('click', cliqueNumero);
-}
-
-function cliqueNumero(event) {
-        if (isNaN(visor.innerHTML)) {
-            visor.innerHTML = event.target.innerHTML;
-        }
-        else {
-            visor.innerHTML = visor.innerHTML + event.target.innerHTML;
-        }
-
-}
-
-btnPonto.addEventListener('click', cliquePonto);
-
-function cliquePonto(event) {
-let valorVisor = Number(visor.innerHTML);
-    if (!isNaN(visor.innerHTML+'.')) {
-        visor.innerHTML = visor.innerHTML + ".";
-    }
-}
-
-
-
-function reset(){
-    visor.innerHTML = "";
-}
-
-for (let i of operador) {
-    i.addEventListener('click', cliqueOperador);
-}
-
+let btnNumeros = document.querySelectorAll(".numeros");
+let visor = document.getElementById("visor");
+let btnC = document.querySelector(".btn-c");
+let operador = document.querySelectorAll(".operador");
+let bntIgual = document.querySelector("#btn-igual");
+let btnPonto = document.querySelector("#btn-ponto");
 let valorSalvo;
 let operacaoSalva;
 
-
-function cliqueOperador(event) {
-
-    if (!isNaN(visor.innerHTML)) {
-        if (valorSalvo === undefined) {
-            
-            valorSalvo = Number(visor.innerHTML);
-        }
-        else {
-            valorSalvo = calculoOperacao(valorSalvo, operacaoSalva, Number(visor.innerHTML));
-        }
-    }
-
-    operacaoSalva = event.target.innerHTML;
-    visor.innerHTML = event.target.innerHTML;
+for (let btn of btnNumeros) {
+  btn.addEventListener("click", cliqueNumero);
 }
 
-bntIgual.addEventListener('click', calcular);
+function cliqueNumero(event) {
+  if (isNaN(visor.innerHTML)) {
+    visor.innerHTML = event.target.innerHTML;
+  }
+  else {
+    visor.innerHTML = visor.innerHTML + event.target.innerHTML;
+  }
+}
+
+btnPonto.addEventListener("click", cliquePonto);
+
+function cliquePonto(event) {
+  let valorVisor = Number(visor.innerHTML);
+  if (!isNaN(visor.innerHTML + ".")) {
+    visor.innerHTML = visor.innerHTML + ".";
+  }
+}
+
+function reset() {
+  visor.innerHTML = "";
+}
+
+for (let i of operador) {
+  i.addEventListener("click", cliqueOperador);
+}
+
+function cliqueOperador(event) {
+    
+        if (!isNaN(visor.innerHTML)) {
+            if (valorSalvo === undefined) {
+              valorSalvo = Number(visor.innerHTML);
+            } else {
+              valorSalvo = calculoOperacao(
+                valorSalvo,
+                operacaoSalva,
+                Number(visor.innerHTML)
+              );
+            }
+          }
+        
+          operacaoSalva = event.target.innerHTML;
+          visor.innerHTML = event.target.innerHTML;
+}
+
+bntIgual.addEventListener("click", calcular);
 
 function calcular(event) {
-    if (valorSalvo != undefined && operacaoSalva != undefined && !isNaN(visor.innerHTML)){
-        visor.innerHTML = calculoOperacao(valorSalvo, operacaoSalva, Number(visor.innerHTML));
-        valorSalvo = undefined;
-        operacaoSalva = undefined;
-    }
-    
+  if (
+    valorSalvo != undefined &&
+    operacaoSalva != undefined &&
+    !isNaN(visor.innerHTML)
+  ) {
+    visor.innerHTML = calculoOperacao(
+      valorSalvo,
+      operacaoSalva,
+      Number(visor.innerHTML)
+    );
+    valorSalvo = undefined;
+    operacaoSalva = undefined;
+  }
 }
 
 function calculoOperacao(valor1, operacao, valor2) {
-    
-    if(operacao === "+") {
-        return valor1+valor2
+  if (operacao === "+") {
+    return valor1 + valor2;
+  } else if (operacao === "-") {
+    return valor1 - valor2;
+  } else if (operacao === "/") {
+    return valor1 / valor2;
+  } else if (operacao === "*") {
+    return valor1 * valor2;
+  }
+}
+
+document.addEventListener("keydown", cliqueTecla);
+
+function cliqueTecla(event) {
+  let tecla = event.key;
+  if (
+    tecla == 0 ||
+    tecla == 1 ||
+    tecla == 2 ||
+    tecla == 3 ||
+    tecla == 4 ||
+    tecla == 5 ||
+    tecla == 6 ||
+    tecla == 7 ||
+    tecla == 8 ||
+    tecla == 9
+  ) {
+    if (isNaN(visor.innerHTML)) {
+      visor.innerHTML = tecla;
+    } else {
+      visor.innerHTML = visor.innerHTML + tecla;
     }
-    else if (operacao === "-") {
-        return valor1-valor2
+  } else if (tecla === "+" || tecla === "-" || tecla === "*" || tecla === "/") {
+    if (!isNaN(visor.innerHTML)) {
+      if (valorSalvo === undefined) {
+        valorSalvo = Number(visor.innerHTML);
+      } else {
+        valorSalvo = calculoOperacao(
+          valorSalvo,
+          operacaoSalva,
+          Number(visor.innerHTML)
+        );
+      }
+      operacaoSalva = tecla;
+      visor.innerHTML = tecla;
     }
-    else if (operacao === "/") {
-        return valor1/valor2
-    }
-    else if (operacao === "*") {
-        return valor1*valor2
-    }
+  } else if (tecla == "Enter" || tecla === "=") {
+    calcular();
+  } else if (tecla == "." || tecla == ",") {
+    cliquePonto();
+  }
+  else if (tecla == "Backspace") {
+    visor.innerHTML = visor.innerHTML.replace(visor.innerHTML[visor.innerHTML.length - 1], "")
+  }
+  else if (tecla == "Escape") {
+    reset();
+  }
 }
 
 
-document.addEventListener('keydown', cliqueTecla);
 
-function cliqueTecla(event) {
-    let tecla = event.key;
-    if ( tecla == 0 || tecla == 1 || tecla == 2 || tecla == 3 || tecla == 4 || tecla == 5 || tecla == 6 || tecla == 7 || tecla == 8 || tecla == 9) {
-        if (isNaN(visor.innerHTML)) {
-            visor.innerHTML = tecla;
-        }
-        else {
-            visor.innerHTML = visor.innerHTML + tecla;
-        }
-    }
-    
-    else if (tecla === '+' || tecla === '-' || tecla === '*' || tecla === '/') {
-        if (!isNaN(visor.innerHTML)) {
-            if (valorSalvo === undefined) {
-                
-                valorSalvo = Number(visor.innerHTML);
-            }
-            else {
-                valorSalvo = calculoOperacao(valorSalvo, operacaoSalva, Number(visor.innerHTML));
-            }
-            operacaoSalva = tecla;
-            visor.innerHTML = tecla;
-        }
-    }
-
-    else if (tecla == 'Enter' || tecla === '=') {
-        calcular();
-    }
-
-    else if (tecla == '.' || tecla == ',') {
-        cliquePonto();
-    }
-} 
