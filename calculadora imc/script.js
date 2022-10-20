bb0fae9065bed1fd7a1b759a6c2aee1d12362bc7
RafaelBochi <rafaelbochibarros@gmail.com>
2022-10-20 14:30:31 -0300

    conclusão

---
+++ calculadora imc/script.js
@@ -0,0 +1,68 @@
+let btnCalc = document.querySelector("button");
+let nome = document.querySelector('.nome-input');
+let altura = document.querySelector('.altura-input');
+let peso = document.querySelector('.peso-input');
+let secImcs = document.querySelector('.imcs');
+
+btnCalc.addEventListener('click', () => calcular ());
+
+const calcular = () => {
+
+    let imc = (peso.value/(altura.value*altura.value)).toFixed(1);
+
+    let imcDiv = document.createElement('div');
+    imcDiv.classList.add('imc')
+
+    if (imc < 15.99) {
+
+        imcDiv.style.backgroundColor = "red";
+        imcDiv.innerHTML = `${nome.value} seu IMC é ${imc}. Você está classificado com MAGREZA ESTREMA`
+        
+    }
+    else if (imc >= 16  && imc <= 16.99) {
+
+        imcDiv.style.backgroundColor = "rgb(255, 60, 0)";
+        imcDiv.innerHTML = `${nome.value} seu IMC é ${imc}. Você está classificado com MAGREZA MODERADA`
+        
+    }
+    else if (imc >= 17 && imc <= 18.5) {
+
+        imcDiv.style.backgroundColor = "rgb(255, 174, 0)";
+        imcDiv.innerHTML = `${nome.value} seu IMC é ${imc}. Você está classificado com MAGREZA LEVE`
+        
+    }
+    else if (imc >= 18.5 && imc <= 24.9) {
+        
+        imcDiv.style.backgroundColor = "rgb(200, 200, 200)";
+        imcDiv.innerHTML = `${nome.value} seu IMC é ${imc}. Você está classificado com SAUDÁVEL`
+
+    }
+    else if (imc >= 25 && imc <= 29.9) {
+        
+        imcDiv.style.backgroundColor = "rgb(255, 174, 0)";
+        imcDiv.innerHTML = `${nome.value} seu IMC é ${imc}. Você está classificado com SOBREPESO`
+
+    }
+    else if (imc >= 30 && imc <= 34.9) {
+        
+        imcDiv.style.backgroundColor = "rgb(255, 60, 0)";
+        imcDiv.innerHTML = `${nome.value} seu IMC é ${imc}. Você está classificado com OBESIDADE GRAU 1`
+
+    }
+    else if ( imc >= 35 && imc <= 39.9) {
+        
+        imcDiv.style.backgroundColor = "rgb(255, 174, 0)";
+        imcDiv.innerHTML = `${nome.value} seu IMC é ${imc}. Você está classificado com OBESIDADE GRAU 2 (Severa)`
+
+    }
+    else if (imc >= 40) {
+        
+        imcDiv.style.backgroundColor = "rgb(0, 0, 0)";
+        imcDiv.style.color = "white"
+        imcDiv.innerHTML = `${nome.value} seu IMC é ${imc}. Você está classificado com OBESIDADE GRAU 3 (Mórbida)`
+
+    }
+
+    secImcs.appendChild(imcDiv)
+
+}
